6c4a17671b573e5b5765c4b80a79e5a25aa68ba4
Nehal Ahmed Mohamed Kamal <nehal.kamal@flairstech.com>
2019-10-01 12:08:00 +0200

    NE-DayComponent

--- src/api/index.js
+++ src/api/index.js
@@ -3,5 +3,5 @@ import axios from 'axios';
 const API_KEY = '49fdf7e9cb61b53a1a2d69d2878b444f';
 
 export default axios.create({
-    baseURL: `http://api.openweathermap.org/data/2.5/q=Cairo&APPID=${API_KEY}&`,
+    baseURL: `http://api.openweathermap.org/data/2.5/forecast?q=Cairo&APPID=${API_KEY}&units=metric`,
     });
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,9 +1,31 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import DayComponent from '../Components/DayComponent';
+import api from '../api/index';
+import '../Components/ComponentStyle.css'
 
-const Home = () => (
-    <div>
-        Home
+const Home = () => {
+    const [mockData, SetMockData] = useState(null)
+    useEffect(() => {
+        api.get().then(function (response) {
+            SetMockData(response.data.list)
+        }).catch(function (error) {
+            console.log(error);
+        })
+        // eslint-disable-next-line
+    }, [])
+    return (<div className='WeatherContainer'>
+        {mockData && mockData.map((item, i) => {
+            if (i % 8 === 0) {
+                return (
+                    <DayComponent key={i} imgSrc="http://openweathermap.org/img/wn/10d@2x.png" response={item} />
+                )
+            } else {
+                return false
+            }
+        })}
     </div>
-)
+    )
+
+}
 
 export default Home;
