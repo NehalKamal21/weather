8955248248ed97e10cc10b9db2d71f837f348fdd
Nehal Ahmed Mohamed Kamal <nehal.kamal@flairstech.com>
2019-10-02 10:54:20 +0200

    Day Component

--- src/components/DayComponent.js
+++ src/components/DayComponent.js
@@ -1,12 +1,13 @@
 import React from 'react';
-import './ComponentStyle.css'
-function DayComponent(props) {
+import './ComponentStyle.css';
+
+
+const DayComponent = (props) => {
     let weekDays = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']
     return (
-
         <div className='dayContainer'>
             {props.response &&
-                <div className='day'>
+                <button className='day'>
                     <div>
                         {props.response && weekDays[new Date(props.response.dt_txt).getDay()]}
                     </div>
@@ -16,11 +17,11 @@ function DayComponent(props) {
                         <span> &deg;C</span> {Math.floor(props.response.main['temp_max'])}
                         <span> &deg;C</span>
                     </div>
-                </div>
+                </button>
             }
-
         </div>
+
     )
 }
-
+    
 export default DayComponent;
